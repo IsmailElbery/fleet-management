0610b069a7e7774e86553fe10e27dab41aafe23a
IElbery <i.elbery@seu.edu.sa>
2024-12-28 18:24:04 +0300

    add Web Auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Web\SeatController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,11 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//add / route to redirect to login page
+Route::get('/', function () {
+    return redirect()->route('register');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/seats', [SeatController::class, 'index'])->name('seats.index');
+    Route::post('/seats/search', [SeatController::class, 'search'])->name('seats.search');
+    Route::post('/seats/book', [SeatController::class, 'book'])->name('seats.book');
+});
 
-Route::get('/', [SeatController::class, 'index'])->name('seats.index');
-Route::get('/seats', [SeatController::class, 'index'])->name('seats.index');
-Route::post('/seats/search', [SeatController::class, 'search'])->name('seats.search');
-Route::post('/seats/book', [SeatController::class, 'book'])->name('seats.book');
-
+require __DIR__.'/auth.php';
